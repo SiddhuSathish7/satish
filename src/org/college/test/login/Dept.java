17b3b8d52a65350737bba61135a423def9ca41e8
SiddhuSathish7 <sportivesathish07@gmail.com>
2021-05-06 21:07:10 +0530

    Section is Added

--- src/org/college/test/login/Dept.java
+++ src/org/college/test/login/Dept.java
@@ -6,6 +6,8 @@ System.out.println("Dept Name is ECE");
 	}
 	public void deptId() {
 		System.out.println("Dept ID is 546");
-
+	}
+	public void deptSection() {
+		System.out.println("Dept Section is A");
 	}
 }
